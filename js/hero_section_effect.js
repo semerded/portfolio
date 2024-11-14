54538bdddba13b58025494b7bddb7006eefdd9ac
Sem VB <sem.van.broekhoven@gmail.com>
2024-11-14 13:33:58 +0100

    created home page hero-section

---
+++ js/hero_section_effect.js
@@ -0,0 +1,16 @@
+const varName = ["--hero-section-bg-bn", "--hero-section-bn", "--hero-section-ds", "--hero-section-sepia"]
+const defaultValue = [0.15, 2, 40, 0.5]
+const maxValue = [1, 1, 0, 0]
+const suffix = ["", "", "px", ""]
+
+window.addEventListener('scroll', function() {
+    const scrollY = window.scrollY;
+    const viewportHeight = window.innerHeight  - document.documentElement.style.getPropertyValue("--navbar-height");
+
+    const scrollPercent = Math.min(scrollY / viewportHeight, 1);
+
+    for (var i = 0; i < varName.length; i++) {
+        const value = defaultValue[i] + scrollPercent * (maxValue[i] - defaultValue[i]);
+        document.documentElement.style.setProperty(varName[i], `${value}${suffix[i]}`);
+    }
+});
