5267e32feef403d8ba2b413cf7690ef58774d8a0
Sem Van Broekhoven <sem.van.broekhoven@gmail.com>
2025-04-24 02:46:36 +0200

    add concept for card

---
+++ src/lib/components/card/card.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import './card.css';
+
+    export let title: string;
+    export let description: string;
+    export let image: string;
+</script>
+
+<div class="card">
+    <div class="card-image">
+        <img src={image} alt={title} />
+    </div>
+    <div class="card-content">
+        <h2>{title}</h2>
+        <p>{description}</p>
+    </div>
+</div>
