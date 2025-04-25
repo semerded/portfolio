bb88c074f3e932205189eb97d10d246453932cbd
Sem Van Broekhoven <sem.van.broekhoven@gmail.com>
2025-04-25 12:53:45 +0200

    update highlighting of active page

--- src/lib/components/navbar/navbar.svelte
+++ src/lib/components/navbar/navbar.svelte
@@ -6,8 +6,8 @@
 <nav>
 	<a href="/"><img src="/logos/logo.webp" alt="logo" /></a>
 	<div>
-		<a href="/about"><span class:nav-active={page.url.pathname === '/about'}>About me</span></a>
-		<a href="/projects"><span class:nav-active={page.url.pathname === '/projects'}>My projects</span></a>
-		<a href="/contact"><span class:nav-active={page.url.pathname === '/contact'}>Contact</span></a>
+		<a href="/about"><span class:nav-active={page.url.pathname.toString().includes('/about')}>About me</span></a>
+		<a href="/projects"><span class:nav-active={page.url.pathname.toString().includes('/projects')}>My projects</span></a>
+		<a href="/contact"><span class:nav-active={page.url.pathname.toString().includes('/contact')}>Contact</span></a>
 	</div>
 </nav>
