cd898a832c12d73ada88c5e9826ce44d4fc923c7
Sem Van Broekhoven <sem.van.broekhoven@gmail.com>
2025-06-03 13:44:30 +0200

    add more languages to proglang

--- src/lib/components/prog-lang.svelte
+++ src/lib/components/prog-lang.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	export let name: string;
 	export let size: number = 1;
+	export let boxShadow: boolean = false;
 
 	const languageData: Record<string, { deviconName: string; fullName: string; color: string }> = {
 		html: {
@@ -72,16 +73,74 @@
 			deviconName: 'arduino',
 			fullName: 'Arduino',
 			color: '#00979d' // Arduino teal
+		},
+		angular: {
+			deviconName: 'angularjs', // Devicon uses 'angularjs' for Angular
+			fullName: 'Angular',
+			color: '#DD0031' // Angular red
+		},
+		svelte: {
+			deviconName: 'svelte',
+			fullName: 'Svelte',
+			color: '#FF3E00' // Svelte orange
+		},
+		ts: {
+			deviconName: 'typescript',
+			fullName: 'TypeScript',
+			color: '#3178C6' // TypeScript blue
+		},
+		docker: {
+			deviconName: 'docker',
+			fullName: 'Docker',
+			color: '#2496ED' // Docker blue
+		},
+		thymeleaf: {
+			deviconName: 'spring', // No Thymeleaf icon in Devicon, using Spring as closest
+			fullName: 'Thymeleaf',
+			color: '#005F0F' // Thymeleaf green
+		},
+		pygame: {
+			deviconName: 'python', // No Pygame icon, using Python
+			fullName: 'Pygame',
+			color: '#3572A5' // Python blue
+		},
+		openGL: {
+			deviconName: 'opengl', // No OpenGL icon in Devicon
+			fullName: 'OpenGL',
+			color: '#5586A4' // OpenGL blue
+		},
+		vscode: {
+			deviconName: 'vscode',
+			fullName: 'VS Code',
+			color: '#007ACC' // VS Code blue
+		},
+		raspberrypi: {
+			deviconName: 'raspberrypi',
+			fullName: 'Raspberry Pi',
+			color: '#C51A4A' // Raspberry Pi red
+		},
+		unknown: {
+			deviconName: 'bub',
+			fullName: 'Unknown',
+			color: 'red'
 		}
 	};
+
+	if (!languageData[name]) {
+		console.error('Given prog lang not in list: ', name);
+		name = 'unknown';
+	}
 </script>
 
-<span class="prog-lang" 
-style="background-color: {languageData[name]['color']}; font-size: {size}rem">
+<span
+	class="prog-lang"
+	class:box-shadow={boxShadow}
+	style="background-color: {languageData[name]['color']}; font-size: {size}rem"
+>
 	<i class="devicon-{languageData[name]['deviconName']}-plain"></i>
-    <span>
-        {languageData[name]['fullName']}
-    </span>
+	<span>
+		{languageData[name]['fullName']}
+	</span>
 </span>
 
 <style>
@@ -89,9 +148,13 @@ style="background-color: {languageData[name]['color']}; font-size: {size}rem">
 		padding: 4px 12px;
 		margin: 5px 5px;
 		border-radius: 10px;
-		box-shadow: 0px 0px 24px 0px gray;
 		display: flex;
 		align-items: center;
+		width: fit-content;
+
+		&.box-shadow {
+			box-shadow: 0px 0px 24px 0px gray;
+		}
 
 		i {
 			margin-right: 4px;
