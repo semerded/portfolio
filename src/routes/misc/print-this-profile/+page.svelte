d69e34aa91e42631de5ce601442218820bbcc4b0
Sem Van Broekhoven <sem.van.broekhoven@gmail.com>
2025-06-13 13:52:10 +0200

    add a page where the profile can be printed

---
+++ src/routes/misc/print-this-profile/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import Profile from '$lib/profile/profile.svelte';
+</script>
+
+<Profile />
+
+<style>
+    
+	@media print {
+        :global(.container-wrapper) {
+        display: block;
+    }
+        :global(:root) {
+            --navbar-height: 0px;
+        }
+		:global(body) {
+			background-color: white;
+		}
+		:global(nav) {
+			display: none !important;
+		}
+      
+	}
+</style>
