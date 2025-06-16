c870eb6f200b2ce7b7bf484bc11ef150d778c88d
Sem Van Broekhoven <sem.van.broekhoven@gmail.com>
2025-06-16 16:47:07 +0200

    add ID to container as param

--- src/lib/components/container/container.svelte
+++ src/lib/components/container/container.svelte
@@ -3,11 +3,12 @@
 
 	export let center: boolean = true;
 	export let minHeight: string | null = null;
+	export let id: string | null = null;
 
 	// export let padding: string = ""
 </script>
 
-<div class="container-wrapper" style={minHeight ? `min-height: ${minHeight}` : ''}>
+<div id="{id}" class="container-wrapper" style={minHeight ? `min-height: ${minHeight}` : ''}>
 	<article class="container">
 		<slot />
 	</article>
