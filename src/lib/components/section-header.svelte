4aff4db1cc54daacdf0000bcbd6fd9137b4fe0ec
Sem Van Broekhoven <sem.van.broekhoven@gmail.com>
2025-06-18 10:36:54 +0200

    update the section header

---
+++ src/lib/components/section-header.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+	export let title: string;
+	export let subtitle: string;
+	export let buttonText: string | null = null;
+	export let href: string | null = null;
+</script>
+
+<div class="section-header">
+	<div class="button"></div>
+	<div class="section-header-text">
+		<h3 class="section-header-subtitle">{subtitle}</h3>
+		<h2 class="section-header-title">{title}</h2>
+	</div>
+	{#if buttonText && href}
+		<a class="button" {href}>{buttonText}</a>
+	{:else}
+		<div class="button"></div>
+	{/if}
+</div>
+
+<style>
+	.section-header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		width: 100%;
+
+		background-color: color-mix(in srgb, var(--bg) 50%, transparent);
+		backdrop-filter: blur(5px);
+		border-radius: 8px;
+		padding: 16px;
+		border: 1px solid rgba(255, 255, 255, 0.2);
+		box-shadow: 2px 2px 24px 0px color-mix(in srgb, var(--box-shadow) 30%, transparent);
+
+		.section-header-text {
+			text-align: center;
+			width: 100%;
+
+			.section-header-title {
+				margin: 0px 0px;
+				padding: 0;
+				font-size: 4rem;
+			}
+
+			.section-header-subtitle {
+				font-size: 1.2rem;
+				font-weight: 500;
+			}
+		}
+
+		.button {
+			width: 200px;
+			margin: 8px;
+			padding: 8px;
+		}
+
+		a {
+			transition: 500ms;
+			background-color: var(--primary);
+			height: 40px;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			border-radius: 8px;
+			color: white;
+			text-decoration: none;
+
+			&:hover {
+				transition: 500ms;
+				background-color: var(--secondary);
+			}
+		}
+	}
+</style>
--- src/lib/components/section-header/section-header.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import './section-header.css';
-
-    export let title: string;
-    export let subtitle: string;
-</script>
-
-<h3 class="section-header-subtitle">{subtitle}</h3>
-<h2 class="section-header-title">{title}</h2>
