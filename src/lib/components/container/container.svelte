9e682de35a0fefba973c1bbe0d8858cdaa08a99f
Sem Van Broekhoven <sem.van.broekhoven@gmail.com>
2025-06-18 10:56:12 +0200

    add margin bottom param to container

--- src/lib/components/container/container.svelte
+++ src/lib/components/container/container.svelte
@@ -4,11 +4,12 @@
 	export let center: boolean = true;
 	export let minHeight: string | null = null;
 	export let id: string | null = null;
+	export let marginBottom: string | null = null;
 
 	// export let padding: string = ""
 </script>
 
-<div id="{id}" class="container-wrapper" style={minHeight ? `min-height: ${minHeight}` : ''}>
+<div id="{id}" class="container-wrapper" style="{minHeight ? `min-height: ${minHeight}` : ''}; {marginBottom ? `margin-bottom: ${marginBottom}` : ''}">
 	<article class="container">
 		<slot />
 	</article>
