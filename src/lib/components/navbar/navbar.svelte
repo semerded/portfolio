7205c8c77a0373217a5c020d4c741db3a6591d89
Sem Van Broekhoven <sem.van.broekhoven@gmail.com>
2025-07-04 18:56:31 +0200

    create page scaffold for website-offers

--- src/lib/components/navbar/navbar.svelte
+++ src/lib/components/navbar/navbar.svelte
@@ -7,6 +7,7 @@
 <nav class="navbar">
 	<a href="/"><img src="/logos/logo.webp" alt="logo" /></a>
 	<div>
+		<a href="/website-offers"><span class:nav-active={page.url.pathname.toString().includes('/website-offers')}>Offers</span></a>
 		<a href="/about"><span class:nav-active={page.url.pathname.toString().includes('/about')}>About me</span></a>
 		<a href="/projects"><span class:nav-active={page.url.pathname.toString().includes('/projects')}>My projects</span></a>
 		<a href="/contact"><span class:nav-active={page.url.pathname.toString().includes('/contact')}>Contact</span></a>
---
+++ src/routes/website-offers/+page.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import Container from '$lib/components/container/container.svelte';
+	import DynamicBackground from '$lib/components/dynamic-background.svelte';
+	import Hero from '$lib/components/hero.svelte';
+	import Offer from '$lib/components/offer.svelte';
+	import './website-offers.css';
+
+	import { json, t } from 'svelte-i18n';
+
+	$: offers = $json('website-offers.offers') as {
+		title: string;
+		price: string;
+		features: string[];
+		benefits: string[];
+	}[];
+</script>
+
+<DynamicBackground image="images/website-offers/bg-dark.jpg" alt="website offers" />
+
+<Hero
+	title="Website offers"
+	description="Let me create a website for you!"
+	buttonText="View my offers"
+	buttonLink="#offers"
+/>
+
+<Container id="offers">
+	{#each offers as offer}
+		<div class="offer-tile">
+			<div class="offer-tile-tile">
+				<Offer
+					title={offer.title}
+					price={offer.price}
+					features={offer.features}
+					benefits={offer.benefits}
+				/>
+			</div>
+			<div class="offer-tile-description">
+				<h1></h1>
+			</div>
+		</div>
+	{/each}
+</Container>
