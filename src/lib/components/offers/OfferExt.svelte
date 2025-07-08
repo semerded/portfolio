ea9ebe48e53d0365ac880d95cd76c9acd631bdce
Sem Van Broekhoven <sem.van.broekhoven@gmail.com>
2025-07-08 23:01:23 +0200

    add text sketch for offer page

---
+++ src/lib/components/offers/OfferExt.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	export let title: string;
+	export let price: string;
+	export let description: string;
+	export let why: { [key: string]: string };
+	export let perfectFor: string;
+	export let slot: string;
+</script>
+
+<section class="offerExt glossy-tile">
+	<div>
+		<h3>{title}</h3>
+		<span class="price">{price}</span>
+		<p>{description}</p>
+		<p>{perfectFor}</p>
+	</div>
+	<div>
+		<ul>
+			{#each Object.entries(why) as [key, value]}
+				<li>
+					{key}<br />
+					<span>&#8618; {value}</span>
+				</li>
+			{/each}
+		</ul>
+		<p>{slot}</p>
+	</div>
+</section>
+
+<style>
+	.offerExt {
+		padding: 1rem;
+		display: flex;
+		flex-wrap: wrap;
+
+		> div {
+			width: 48%;
+			margin: 1%;
+
+			li {
+				span {
+                    margin-left: 1rem;
+				}
+			}
+		}
+	}
+</style>
--- src/routes/website-offers/+page.svelte
+++ src/routes/website-offers/+page.svelte
@@ -2,44 +2,34 @@
 	import Container from '$lib/components/container/container.svelte';
 	import DynamicBackground from '$lib/components/dynamic-background.svelte';
 	import Hero from '$lib/components/hero.svelte';
-	import Offer from '$lib/components/offer.svelte';
+	import OfferExt from '$lib/components/offers/OfferExt.svelte';
 	import './website-offers.css';
 
 	import { json, t } from 'svelte-i18n';
 
-	$: offers = $json('website-offers.offers') as {
+	$: offers = $json('offers') as {
 		title: string;
 		price: string;
-		features: string[];
-		benefits: string[];
+		description: string;
+		perfectFor: string;
+		why: {[key: string]: string };
+		slot: string;
 	}[];
 </script>
 
 <DynamicBackground image="images/website-offers/bg-dark.jpg" alt="website offers" />
 
 <Hero
-	title="Website offers"
-	description="Let me create a website for you!"
-	buttonText="View my offers"
+	title={$t('title')}
+	description={$t('subtitle')}
+	buttonText={$t('button')}
 	buttonLink="#offers"
 />
 
 <main>
 	<Container id="offers">
 		{#each offers as offer}
-			<div class="offer-tile">
-				<div class="offer-tile-tile">
-					<Offer
-						title={offer.title}
-						price={offer.price}
-						features={offer.features}
-						benefits={offer.benefits}
-					/>
-				</div>
-				<div class="offer-tile-description">
-					<h1></h1>
-				</div>
-			</div>
+			<OfferExt {...offer} />
 		{/each}
 	</Container>
 </main>
