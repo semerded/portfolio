2fa098dcdcdfc306e68ad09b117ec97deb3f98c2
Sem Van Broekhoven <sem.van.broekhoven@gmail.com>
2025-07-16 13:50:52 +0200

    add i18n to navbar

--- src/lib/components/navbar/navbar.svelte
+++ src/lib/components/navbar/navbar.svelte
@@ -3,15 +3,16 @@
 	import { page } from '$app/state';
 	import LightDarkModeSwitch from '$lib/components/light-dark-mode-switch.svelte';
 	import ToggleLanguage from '../ToggleLanguage.svelte';
+	import { _ } from 'svelte-i18n';
 </script>   
 
 <nav class="navbar">
 	<a href="/"><img src="/logos/logo.webp" alt="logo" /></a>
 	<div>
-		<a href="/website-offers"><span class:nav-active={page.url.pathname.toString().includes('/website-offers')}>Offers</span></a>
-		<a href="/about"><span class:nav-active={page.url.pathname.toString().includes('/about')}>About me</span></a>
-		<a href="/projects"><span class:nav-active={page.url.pathname.toString().includes('/projects')}>My projects</span></a>
-		<a href="/contact"><span class:nav-active={page.url.pathname.toString().includes('/contact')}>Contact</span></a>
+		<a href="/website-offers"><span class:nav-active={page.url.pathname.toString().includes('/website-offers')}>{$_('navbar.offers')}</span></a>
+		<a href="/about"><span class:nav-active={page.url.pathname.toString().includes('/about')}>{$_('navbar.about')}</span></a>
+		<a href="/projects"><span class:nav-active={page.url.pathname.toString().includes('/projects')}>{$_('navbar.projects')}</span></a>
+		<a href="/contact"><span class:nav-active={page.url.pathname.toString().includes('/contact')}>{$_('navbar.contact')}</span></a>
 		<div>
 			<LightDarkModeSwitch></LightDarkModeSwitch>
 			<ToggleLanguage></ToggleLanguage>
