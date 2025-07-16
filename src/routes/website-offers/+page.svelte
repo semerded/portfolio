518a407e6520537d60735ba856bf44d03b3d2edd
Sem Van Broekhoven <sem.van.broekhoven@gmail.com>
2025-07-16 13:55:58 +0200

    create component from offer form

--- src/routes/website-offers/+page.svelte
+++ src/routes/website-offers/+page.svelte
@@ -8,6 +8,7 @@
 	import './website-offers.css';
 
 	import { json, t } from 'svelte-i18n';
+	import OfferForm from '$lib/components/form/OfferForm.svelte';
 
 	$: offers = $json('offers') as {
 		title: string;
@@ -33,25 +34,6 @@
 		} else {
 			throw new RangeError('selected offer is not in the range of the options: ' + index);
 		}
-	}
-
-	let name = '';
-	let email = '';
-	let offer = -1;
-	let validFormPart = { name: false, email: false, offer: false };
-	$: validForm = Object.values(validFormPart).every(Boolean);
-
-	function validateName(val: string) {
-		validFormPart.name = val.trim().length > 1;
-	}
-
-	function validateEmail(val: string) {
-		// Simple email regex
-		validFormPart.email = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(val);
-	}
-
-	function validateOffer(index: number) {
-		validFormPart.offer = index !== -1 && index < offers.length;
 	}
 </script>
 
@@ -139,78 +121,8 @@
 				<p>{$t('form.optional')}</p>
 				<p>{$t('form.note')}</p>
 			</div>
-			<form>
-				<div>
-					<input
-						type="text"
-						bind:value={name}
-						placeholder={$t('form.placeholder.name')}
-						required
-						on:input={() => validateName(name)}
-					/>
-					<i
-						class="fa-solid form-checkmark"
-						class:fa-check-circle={validFormPart.name}
-						class:fa-flip={validFormPart.name}
-						class:fa-circle-xmark={!validFormPart.name}
-					></i>
-				</div>
 
-				<div>
-					<input
-						type="text"
-						bind:value={email}
-						placeholder={$t('form.placeholder.email')}
-						required
-						on:input={() => validateEmail(email)}
-					/>
-					<i
-						class="fa-solid form-checkmark"
-						class:fa-check-circle={validFormPart.email}
-						class:fa-flip={validFormPart.email}
-						class:fa-circle-xmark={!validFormPart.email}
-					></i>
-				</div>
-
-				<div>
-					<select
-						name=""
-						id="select-offer"
-						required
-						class:placeholder={offer == -1}
-
-						bind:value={offer}
-						on:change={() => validateOffer(offer)}
-					>
-						<option value={-1} disabled selected hidden>{$t('form.placeholder.offer')}</option>
-						{#each offers as offer, index}
-							<option value={index}>{offer.title}</option>
-						{/each}
-						<option value={-2}>{$t('form.cant-decide')}</option>
-					</select>
-					<i
-						class="fa-solid form-checkmark"
-						class:fa-check-circle={validFormPart.offer}
-						class:fa-flip={validFormPart.offer}
-						class:fa-circle-xmark={!validFormPart.offer}
-					></i>
-				</div>
-
-				<div>
-					<textarea name="" placeholder={$t('form.placeholder.message')} id="form-message"
-					></textarea>
-				</div>
-
-				<div>
-					<button
-						class="button"
-						disabled={!validForm}
-						on:click={() => {
-							console.log('send data');
-						}}>{$t('form.button')}</button
-					>
-				</div>
-			</form>
+			<OfferForm {offers} />
 		</div>
 	</Container>
 </main>
@@ -254,88 +166,6 @@
 
 			p {
 				padding: 8px 0;
-			}
-		}
-
-		form {
-			max-width: 500px;
-			width: 100%;
-			display: flex;
-			flex-direction: column;
-			gap: 8px;
-
-			div {
-				position: relative;
-				display: flex;
-				align-items: center;
-
-				i {
-					--size: 24px;
-					--fa-animation-iteration-count: 1;
-					position: absolute;
-					font-size: var(--size);
-					right: calc((var(--size) + 4px) * -1);
-					color: red;
-
-					&.fa-check-circle {
-						color: yellowgreen;
-					}
-				}
-			}
-
-			button {
-				width: 100%;
-				font-weight: 600;
-				&:disabled {
-					opacity: 0.5;
-					color: black;
-					background-color: lightgray;
-					cursor: not-allowed;
-				}
-			}
-
-			input,
-			textarea,
-			select {
-				width: 100%;
-				padding: 8px 4px;
-				background-color: transparent;
-				border: 2px solid var(--secondary);
-				border-radius: 8px;
-				font-size: 1.1rem;
-				color: var(--text);
-
-				&::placeholder, &.placeholder {
-					color: rgb(184, 180, 180);
-					font-style: italic;
-				}
-
-				&:focus {
-					outline: unset;
-					box-shadow: 0px 0px 16px 0px var(--secondary);
-
-					&[required] {
-						box-shadow: 0px 0px 16px 0px var(--primary);
-					}
-				}
-
-				&[required] {
-					border: 2px solid var(--primary);
-				}
-			}
-
-			textarea {
-				resize: vertical;
-				min-height: 150px;
-				max-height: 200px;
-			}
-
-			option {
-				background-color: var(--card);
-
-				&:active {
-					background-color: var(--secondary);
-				}
 			}
 		}
 	}
