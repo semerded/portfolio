e63f43832b23fe7ba270b26af97707edfed17190
Sem VB <sem.van.broekhoven@gmail.com>
2025-08-18 12:31:52 +0200

    add an observer to only make the image render when slit is visible

--- src/lib/components/StaticBgContainer.svelte
+++ src/lib/components/StaticBgContainer.svelte
@@ -1,6 +1,21 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+
 	export let imageUrl: string;
 	export let paddingY: string = '4rem';
+
+	let observer: IntersectionObserver;
+	let visible: boolean = true;
+	let slitContainer: HTMLElement;
+
+	onMount(() => {
+		observer = new IntersectionObserver((entries) => {
+			visible = entries[0].isIntersecting;
+		});
+		slitContainer = document.querySelector('.static-bg-container')!;
+		observer.observe(slitContainer);
+		return () => observer.disconnect();
+	});
 </script>
 
 <div
@@ -8,9 +23,11 @@
 	class="static-bg-container {$$restProps.class}"
 	style="padding-top: {paddingY}; padding-bottom: {paddingY};"
 >
-	<div class="image-container">
-		<img src={imageUrl} alt="static background" />
-	</div>
+	{#if visible}
+		<div class="image-container">
+			<img src={imageUrl} alt="static background" />
+		</div>
+	{/if}
 	<slot />
 </div>
 
